b1d12b91aef02d266ee5892ad003a530b2ce6bb8
dedeepya55 <22A91A05C8@aec.edu.in>
2026-03-08 17:56:01 +0530

    Add user api

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,3 +29,37 @@ exports.updateProfile = async (req, res) => {
     res.status(500).json({ message: "Profile update failed" });
   }
 };
+
+
+// 🔹 ADD NEW USER (without hashing password)
+exports.addUser = async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+
+    // check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const newUser = new User({
+      email,
+      password, // stored directly
+      role
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      message: "User created successfully",
+      user: {
+        id: newUser._id,
+        email: newUser.email,
+        role: newUser.role
+      }
+    });
+
+  } catch (err) {
+    res.status(500).json({ message: "Failed to create user" });
+  }
+};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,7 +4,7 @@ const { changePassword } = require("../controllers/authController");
 
 const authMiddleware = require("../middleware/authMiddleware");
 const profileUpload = require("../middleware/profileUpload");
-const { getProfile, updateProfile } = require("../controllers/userController");
+const { getProfile, updateProfile, addUser} = require("../controllers/userController");
 
 const router = express.Router();
 
@@ -24,5 +24,7 @@ router.put(
 );
 
 router.put("/change-password", authMiddleware, changePassword);
+
+router.post("/add-user", authMiddleware, addUser);
 
 module.exports = router;
